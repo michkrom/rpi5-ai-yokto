f7d0a66edc5657b186054fb70d6c63aedcd91d03
Yokto Developer <dev@yokto.local>
2026-05-30 11:52:58 -0700

    fix: add SDL_VIDEO_GL_DRIVER to force GLES library loading

--- layers/meta-doom/recipes-games/chocolate-doom/files/chocolate-doom-wrapper.sh
+++ layers/meta-doom/recipes-games/chocolate-doom/files/chocolate-doom-wrapper.sh
@@ -10,6 +10,8 @@ export SDL_RENDER_DRIVER=opengles2
 export XDG_RUNTIME_DIR=/run/user/1000
 export WAYLAND_DISPLAY=wayland-1
 
+# Force SDL to use the correct GLES library path
+export SDL_VIDEO_GL_DRIVER=/usr/lib/libGLESv2.so.2
+
 # Force windowed mode - fullscreen doesn't work well with Wayland/EGL in chocolate-doom
-# This prevents the "Error creating window for video startup: EGL not initialized" error
 exec /usr/bin/chocolate-doom -nofullscreen -window "$@"
